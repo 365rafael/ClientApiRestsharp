d2e4d7b0f513ae0af31640ce931b1ac768027f28
Rafael Arantes <rafael-fullstack@outlook.com>
2024-06-28 15:42:17 -0300

    Adicionar arquivos de projeto.

---
+++ Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApiRestsharp
+{
+    internal class Car
+    {
+        public Car(string name, int year)
+        {
+            Name = name;
+            Year = year;
+        }
+
+        public string Name { get; set; } = default!;
+        public int Year { get; set; }
+
+        //Sempre que referenciar Car.ToString, vai listar o nome e ano do carro
+        public override string ToString()
+        {
+            return $"{Name} - {Year}";
+        }
+    }
+}
